51189775d83702a82eaaf9b6401d12b16f72b6f8
Levi Eber <gblevieber@gmail.com>
2023-11-24 18:54:00 -0300

    feat: add cors and helmet

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import { DASHBOARD_ORIGIN } from "@/config/environment-variables";
 import { errorHandler } from "@/middlewares/error-handler";
 import { authenticate } from "@/middlewares/authentication-middleware";
 import { productRoutes } from "@/routes/product-routes";
 import { authRoutes } from "@/routes/auth-routes";
 
 export const app = Fastify({
-	logger: process.env.NODE_ENV === "development",
+  logger: process.env.NODE_ENV === "development",
 });
 
-app.setErrorHandler(errorHandler)
+app.setErrorHandler(errorHandler);
 
-app.register(authenticate)
+app.register(helmet, { global: true });
 
-app.register(authRoutes)
+console.log(DASHBOARD_ORIGIN)
 
-app.register(productRoutes)
+app.register(cors, {
+  origin: process.env.NODE_ENV === "development" ? "*" : DASHBOARD_ORIGIN,
+});
+
+app.register(authenticate);
+
+app.register(authRoutes);
+
+app.register(productRoutes);
--- src/config/environment-variables.ts
+++ src/config/environment-variables.ts
@@ -3,3 +3,7 @@ import * as env from "env-var";
 export const DATABASE_URL = env.get("DATABASE_URL").required().asString();
 export const PORT = env.get("PORT").default(3333).asPortNumber();
 export const JWT_SECRET_KEY = env.get("JWT_SECRET_KEY").required().asString();
+export const DASHBOARD_ORIGIN = env
+  .get("DASHBOARD_ORIGIN")
+  .required(process.env.NODE_ENV !== "development")
+  .asUrlString();
